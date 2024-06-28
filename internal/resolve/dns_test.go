01b5a3c47ace827625400bf09b1798d0613964fc
Vladimir Komissarov <dr0id@dr0id.ru>
2024-06-28 18:51:50 +0700

    adds support for customizing nameservers

--- internal/resolve/dns_test.go
+++ internal/resolve/dns_test.go
@@ -2,14 +2,25 @@ package resolve
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestResolveDNS(t *testing.T) {
-	addresses, err := resolveDNS(context.Background(), "example.com.")
+	configs := []DnsConfig{
+		{Nameservers: nil},
+	}
 
-	require.NoError(t, err)
-	require.GreaterOrEqual(t, len(addresses), 1, "Expected at least one address")
+	for _, conf := range configs {
+		resolver := newDNSResolver(DnsConfig{})
+		addresses, err := resolver.resolve(context.Background(), "example.com.")
+
+		require.NoError(t, err, fmt.Sprintf("configuration: %v", conf))
+		require.GreaterOrEqual(
+			t, len(addresses), 1,
+			fmt.Sprintf("Expected at least one address; configuration: %v", conf),
+		)
+	}
 }
